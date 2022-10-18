7434193fe48810ba56de4be25e8124027f2c25fd
Mutesi <ktesialine@gmail.com>
2022-10-18 11:56:32 +0200

    mobile and desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { "light-gray": "rgb(242,242,242)" },
+      boxShadow:{ 'main-shadow' :'0 1px 6px rgb(32 33 36 / 28%);'},
+    },
   },
   plugins: [],
 };
