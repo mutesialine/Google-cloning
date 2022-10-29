61dcc06bcc5368c194b3a1eb8f6840e1327ea75a
Mutesi <ktesialine@gmail.com>
2022-10-29 13:03:05 +0200

    fixing responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,24 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: { "light-gray": "rgb(242,242,242)" },
-      boxShadow:{ 'main-shadow' :'0 1px 6px rgb(32 33 36 / 28%);'},
+      fontFamily: {
+        sans: ["google", "sans-serif"],
+      },
+      colors: {
+        primary: " #4d5156",
+        "light-gray": "#f2f2f2",
+        "middle-gray": "#70757a",
+        "dark-blue": "#1a0dab"
+      },
+      width:{
+        "nigthy-percent":"92%",
+        "fourty-eigt":"48%"
+      },
+      fontSize: {
+        thirteen: "13px",
+        fifteen: "15px",
+      },
+      boxShadow: { "main-shadow": "0 1px 6px rgb(32 33 36 / 28%);" },
     },
   },
   plugins: [],
